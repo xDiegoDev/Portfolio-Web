0c4622bb7777751d8503cba1446d00a53cb2d784
Diego Prieto <diego.prieto.dev@gmail.com>
2023-06-01 10:32:24 +0200

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,9 +43,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+          <p className="ml-3 text-white text-[18px] font-bold cursor-pointer flex ">
             Diego &nbsp;
-            <span className="sm:block hidden"> | Prieto Iñiguez</span>
+            <span className="sm:block hidden"> | Front Dev</span>
           </p>
         </Link>
 
